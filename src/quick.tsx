49ca24320cb62768e8c19792324e9332f47b75bd
toriel <ruinstoriel@proton.me>
2024-06-01 15:37:14 +0800

    完成

--- src/quick.tsx
+++ src/quick.tsx
@@ -1,7 +1,12 @@
 import { useState } from 'react';
-export default function Board(){
-    const [squares, setSquares] = useState<string[]>(Array(9).fill(''));
-    const [xIsNext, setXIsNext] = useState(true);
+
+interface BoardProps {
+    xIsNext:boolean
+    squares: string[]
+    onPlay: (nextSquares:string[])=>void
+}
+const Board: React.FC<BoardProps> = ({xIsNext, squares, onPlay}) =>{
+
     function handleClick(i:number) {
         if (squares[i] || calculateWinner(squares)) {
             return;
@@ -12,8 +17,7 @@ export default function Board(){
           } else {
             nextSquares[i] = "O";
           }
-        setSquares(nextSquares);
-        setXIsNext(!xIsNext);
+          onPlay(nextSquares)
     }
     const winner = calculateWinner(squares);
     let status;
@@ -43,6 +47,7 @@ export default function Board(){
         </>
       );
 }
+
 interface SquareProps {
     value:string
     onSquareClick: ()=>void
@@ -70,4 +75,46 @@ interface SquareProps {
       }
     }
     return null;
+  }
+
+  export default function Game() {
+    
+    const [history, setHistory] = useState([Array(9).fill(null)]);
+    const [currentMove, setCurrentMove] = useState(0);
+    const xIsNext = currentMove % 2 === 0;
+    const currentSquares = history[currentMove];
+    function handlePlay(nextSquares:string[]) {
+        const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+        setHistory(nextHistory);
+        setCurrentMove(nextHistory.length - 1);
+    }
+
+    function jumpTo(nextMove:number) {
+        setCurrentMove(nextMove);
+        // TODO
+      }
+    
+      const moves = history.map((squares, move) => {
+        let description;
+        if (move > 0) {
+          description = 'Go to move #' + move;
+        } else {
+          description = 'Go to game start';
+        }
+        return (
+          <li key={move}>
+            <button onClick={() => jumpTo(move)}>{description}</button>
+          </li>
+        );
+      });
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+        </div>
+        <div className="game-info">
+          <ol>{moves}</ol>
+        </div>
+      </div>
+    );
   }
