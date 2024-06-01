6ec40532dbf136a5e9250d2a0fd87c2f2fc76c7f
toriel <ruinstoriel@proton.me>
2024-06-01 14:18:19 +0800

    状态提升到父组件

--- src/quick.tsx
+++ src/quick.tsx
@@ -1,35 +1,32 @@
 import { useState } from 'react';
 export default function Board(){
+    const [squares, setSquares] = useState<string[]>(Array(9).fill(''));
     return (
         <>
           <div className="board-row">
-            <Square  />
-            <Square  />
-            <Square />
+            <Square  value={squares[0]}/>
+            <Square  value={squares[1]}/>
+            <Square value={squares[2]}/>
           </div>
           <div className="board-row">
-            <Square />
-            <Square />
-            <Square />
+            <Square value={squares[3]}/>
+            <Square value={squares[4]}/>
+            <Square value={squares[5]}/>
           </div>
           <div className="board-row">
-            <Square />
-            <Square />
-            <Square />
+            <Square value={squares[6]}/>
+            <Square value={squares[7]}/>
+            <Square value={squares[8]}/>
           </div>
         </>
       );
 }
 interface SquareProps {
-
+    value:string
 }
   
-  const Square: React.FC<SquareProps> = () => {
-    const [value, setValue] = useState<string>('');
-    function handleClick() {
-        setValue("X")
-    }
+  const Square: React.FC<SquareProps> = ({value}) => {
 
-    return <button className="square" onClick={handleClick}>{value}</button>;
+    return <button className="square">{value}</button>;
   };
   
